8ceeff7c36c2df6569d02271bdc7f8349b5363a9
caroline burns <carolineburns@carolines-MacBook.local>
2019-01-21 17:33:01 -0700

    setting state and made form for questions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,107 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      results: [],
+      girl: '',
+      picture: '',
+      reason: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://young-anchorage-78332.herokuapp.com/data')
+    .then(response => response.json())
+    
+    .then(myJson => {
+      this.setState({
+        results: myJson.data.results
+      })
+    });
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="form-group">
+        <div className="form-group">
+          <label for="questionOne">Your Burn Book contains:</label>
+          <select className="form-control" id="questionOne">
+            <option>Dirty Secrets</option>
+            <option>Gossip</option>
+            <option>A list of people you dislike</option>
+            <option>A list of people you want to kiss</option>
+            <option>Risquex pictures</option>
+            <option>Your secret weed stash</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label for="questionTwo">Your extracurricular activity is:</label>
+          <select className="form-control" id="questionTwo">
+            <option>Yearbook</option>
+            <option>Mathletes</option>
+            <option>Lacrosse</option>
+            <option>Cheerleading</option>
+            <option>Getting high under the bleachers</option>
+            <option>Spring Fling committee</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label for="questionThree">What is in your locker?</label>
+          <select className="form-control" id="questionThree">
+            <option>An old sandwich</option>
+            <option>A mirror and lip gloss</option>
+            <option>Textbooks</option>
+            <option>Your other weed stash</option>
+            <option>Art supplies</option>
+            <option>Foot cream</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label for="questionFour">What did your last text message say?</label>
+          <select className="form-control" id="questionFour">
+            <option>"Aaron Samuels looks sexy with his hair pushed back"</option>
+            <option>"That's soo fetch"</option>
+            <option>"I have some major plastic sabotage planned"</option>
+            <option>"Did you hear about Coach Carr?"</option>
+            <option>"Wanna go to Taco Bell?"</option>
+            <option>"I want to loose three pounds"</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label for="questionFive">What would you do for the Winter Talent Show?</label>
+          <select className="form-control" id="questionfive">
+            <option>Sing a song</option>
+            <option>Do a rap</option>
+            <option>A group dance</option>
+            <option>Juggling</option>
+            <option>A Shakespearean monologue</option>
+            <option>Play a musical instrument</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label for="questionSix">What should the theme of the Spring Fling be?</label>
+          <select className="form-control" id="questionSix">
+            <option>Circus</option>
+            <option>Great Gatsby</option>
+            <option>Hollywood</option>
+            <option>Arabian Nights</option>
+            <option>Burning Man</option>
+            <option>Parisian Nights</option>
+          </select>
+        </div>
       </div>
-    );
+    )
+    
   }
 }
+
+
 
 export default App;
